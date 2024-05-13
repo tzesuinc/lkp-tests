tzesuinc/lkp-tests

commit dd9da9fb91633d3a4d6e352a026e8a22ec49819f

lib/lkp_pattern.rb: load pattern by removing line started with # or empty
Signed-off-by: Philip Li <philip.li@intel.com>

lib/lkp_pattern.rb
@@ -9,26 +9,36 @@ require "#{LKP_SRC}/lib/ruby_ext"
 
 module LKP
   class Pattern
-    attr_reader :regexp, :file
+    attr_reader :file
 
     def initialize(file)
       @file = file
-
-      load
     end
 
     def contain?(content)
       return unless regexp
 
-      content =~ regexp
+      Array(content).any? { |line| line =~ regexp }
     end
 
-    def load
-      @regexp = load_regular_expressions(file) if File.size?(file)
+    def regexp
+      return @regexp if @regexp
+      return unless File.size?(file)
+
+      lines = File.readlines(file)
+                  .map(&:chomp)
+                  .reject(&:empty?)
+                  .reject { |line| line.start_with?('#') }
+
+      @regexp = Regexp.new lines.join('|')
     end
 
     def patterns
       File.read(file).split("\n")
+    end
+
+    def pattern(content)
+      patterns.find { |pattern| content =~ Regexp.new(pattern) }
     end
 
     class << self

commit 669f7a45c0062a7c2b46fe9fbcd3bf260129f6c9

lib/lkp_pattern.rb: add parentheses for regexp to obtain the matching fragment
In commit dd9da9fb9163, new code was introduced to replace
load_regular_expressions method, but missed to add the surrounding
parentheses, so other code cannot obtain the matching fragment, leading
to many spec failures.

Add the parentheses back to obtain the matching fragment correctly.

== fix issue ==

[    0.861994] sysctl could not get directory: /debug/test_sysctl/mnt/mnt_error -30
----------------

Traceback (most recent call last):
	3: from /lkp/yujie/src/stats/dmesg:107:in `<main>'
	2: from /lkp/yujie/src/stats/dmesg:107:in `each'
	1: from /lkp/yujie/src/stats/dmesg:142:in `block in <main>'
/lkp/yujie/src/lib/dmesg.rb:239:in `analyze_error_id': undefined method `sub' for nil:NilClass (NoMethodError)

Add another spec to ensure the error is correctly captured and parsed.

Fixes: dd9da9fb9163 ("lib/lkp_pattern.rb: load pattern by removing line started with # or empty")
Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/lkp_pattern.rb
@@ -30,7 +30,7 @@ module LKP
                   .reject(&:empty?)
                   .reject { |line| line.start_with?('#') }
 
-      @regexp = Regexp.new lines.join('|')
+      @regexp = Regexp.new "(#{lines.join('|')})"
     end
 
     def patterns

spec/stats/dmesg/82
@@ -1,0 +1,1 @@
+[    0.861994] sysctl could not get directory: /debug/test_sysctl/mnt/mnt_error -30

spec/stats/dmesg/82.yaml
@@ -1,0 +1,9 @@
+boot_failures: 1
+
+# sysctl could not get directory:
+sysctl_could_not_get_directory: 1
+message:sysctl_could_not_get_directory: [    0.861994] sysctl could not get directory: /debug/test_sysctl/mnt/mnt_error -30
+pattern:sysctl_could_not_get_directory: sysctl could not get directory:
+
+timestamp:last: 0.861994
+timestamp:sysctl_could_not_get_directory: 0.861994