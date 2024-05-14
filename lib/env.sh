36348b1a54caf84dfb00c5619567c7b2347aa4d0
Yujie Liu <yujie.liu@intel.com>
2024-05-14 09:35:25 +0800

    lib/env.sh: fix iptables binary path
    It should be under /usr/sbin instead of /usr/bin
    
    Fixes: 51064b4a146f ("lib/env.sh: set iptables path")
    Signed-off-by: Yujie Liu <yujie.liu@intel.com>
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/env.sh
+++ lib/env.sh
@@ -93,13 +93,13 @@ set_iptables_path()
 	do
 		has_cmd $iptables_bin || {
 			if has_cmd $iptables_bin-nft; then
-				ln -sfv $(cmd_path $iptables_bin-nft) /usr/bin/$iptables_bin
-				ln -sfv $(cmd_path $iptables_bin-nft-restore) /usr/bin/$iptables_bin-restore
-				ln -sfv $(cmd_path $iptables_bin-nft-save) /usr/bin/$iptables_bin-save
+				ln -sfv $(cmd_path $iptables_bin-nft) /usr/sbin/$iptables_bin
+				ln -sfv $(cmd_path $iptables_bin-nft-restore) /usr/sbin/$iptables_bin-restore
+				ln -sfv $(cmd_path $iptables_bin-nft-save) /usr/sbin/$iptables_bin-save
 			elif has_cmd $iptables_bin-legacy; then
-				ln -sfv $(cmd_path $iptables_bin-legacy) /usr/bin/$iptables_bin
-				ln -sfv $(cmd_path $iptables_bin-legacy-restore) /usr/bin/$iptables_bin-restore
-				ln -sfv $(cmd_path $iptables_bin-legacy-save) /usr/bin/$iptables_bin-save
+				ln -sfv $(cmd_path $iptables_bin-legacy) /usr/sbin/$iptables_bin
+				ln -sfv $(cmd_path $iptables_bin-legacy-restore) /usr/sbin/$iptables_bin-restore
+				ln -sfv $(cmd_path $iptables_bin-legacy-save) /usr/sbin/$iptables_bin-save
 			fi
 		}
 	done

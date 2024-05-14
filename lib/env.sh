tzesuinc/lkp-tests

commit 51064b4a146fa80c5fd531833397107ecf8c06bc

lib/env.sh: set iptables path
iptables has two versions based on different kernel interfaces. The
iptables-legacy uses old getsockopt/setsockopt interface, while the
iptables-nft uses the newer nf_tables interface.

If directly do "apt install iptables", iptables will be softlinked
to the nft version automatically.

root@9c6f46c64d68:~# apt install iptables
root@9c6f46c64d68:~# command -v iptables
/usr/sbin/iptables
root@9c6f46c64d68:~# ls -l /usr/sbin/iptables
lrwxrwxrwx 1 root root 26 Jan 16  2023 /usr/sbin/iptables -> /etc/alternatives/iptables
root@9c6f46c64d68:~# ls -l /etc/alternatives/iptables
lrwxrwxrwx 1 root root 22 May 13 05:05 /etc/alternatives/iptables -> /usr/sbin/iptables-nft
root@9c6f46c64d68:~# ls -l /usr/sbin/iptables*
lrwxrwxrwx 1 root root   26 Jan 16  2023 /usr/sbin/iptables -> /etc/alternatives/iptables
-rwxr-xr-x 1 root root 7037 Jan 12  2023 /usr/sbin/iptables-apply
lrwxrwxrwx 1 root root   20 Jan 16  2023 /usr/sbin/iptables-legacy -> xtables-legacy-multi
lrwxrwxrwx 1 root root   20 Jan 16  2023 /usr/sbin/iptables-legacy-restore -> xtables-legacy-multi
lrwxrwxrwx 1 root root   20 Jan 16  2023 /usr/sbin/iptables-legacy-save -> xtables-legacy-multi
lrwxrwxrwx 1 root root   17 Jan 16  2023 /usr/sbin/iptables-nft -> xtables-nft-multi
lrwxrwxrwx 1 root root   17 Jan 16  2023 /usr/sbin/iptables-nft-restore -> xtables-nft-multi
lrwxrwxrwx 1 root root   17 Jan 16  2023 /usr/sbin/iptables-nft-save -> xtables-nft-multi
lrwxrwxrwx 1 root root   34 Jan 16  2023 /usr/sbin/iptables-restore -> /etc/alternatives/iptables-restore
lrwxrwxrwx 1 root root   17 Jan 16  2023 /usr/sbin/iptables-restore-translate -> xtables-nft-multi
lrwxrwxrwx 1 root root   31 Jan 16  2023 /usr/sbin/iptables-save -> /etc/alternatives/iptables-save
lrwxrwxrwx 1 root root   17 Jan 16  2023 /usr/sbin/iptables-translate -> xtables-nft-multi

In packing process, we will first call "apt-get download" and then
"dpkg-deb" to install it. In this case, although nft and legacy iptables

lib/env.sh
@@ -87,6 +87,24 @@ set_perf_path()
 	fi
 }
 
+set_iptables_path()
+{
+	for iptables_bin in iptables ip6tables
+	do
+		has_cmd $iptables_bin || {
+			if has_cmd $iptables_bin-nft; then
+				ln -sfv $(cmd_path $iptables_bin-nft) /usr/bin/$iptables_bin
+				ln -sfv $(cmd_path $iptables_bin-nft-restore) /usr/bin/$iptables_bin-restore
+				ln -sfv $(cmd_path $iptables_bin-nft-save) /usr/bin/$iptables_bin-save
+			elif has_cmd $iptables_bin-legacy; then
+				ln -sfv $(cmd_path $iptables_bin-legacy) /usr/bin/$iptables_bin
+				ln -sfv $(cmd_path $iptables_bin-legacy-restore) /usr/bin/$iptables_bin-restore
+				ln -sfv $(cmd_path $iptables_bin-legacy-save) /usr/bin/$iptables_bin-save
+			fi
+		}
+	done
+}
+
 disable_nmi_watchdog()
 {
 	# Disable NMI watchdog to free up one perf counter

lib/tests/kernel-selftests.sh
@@ -124,10 +124,8 @@ prepare_for_commands()
 		llvm_ver=${llvm##*/}
 		export PATH=$PATH:/usr/lib/$llvm_ver/bin
 	}
-	# fix sh: 1: iptables: not found
-	has_cmd iptables || log_cmd ln -sf /usr/sbin/iptables-nft /usr/bin/iptables
-	# fix ip6tables: command not found
-	has_cmd ip6tables || log_cmd ln -sf /usr/sbin/ip6tables-nft /usr/bin/ip6tables
+
+	set_iptables_path
 }
 
 check_kconfig()

lib/tests/ltp.sh
@@ -96,11 +96,7 @@ workaround_env()
 		fi
 	}
 
-	# fix CONF: 'iptables' not found
-	has_cmd iptables || log_cmd ln -sf /usr/sbin/iptables-nft /usr/bin/iptables
-
-	# fix CONF: 'ip6tables' not found
-	has_cmd ip6tables || log_cmd ln -sf /usr/sbin/ip6tables-nft /usr/bin/ip6tables
+	set_iptables_path
 }
 
 specify_tmpdir()

commit 36348b1a54caf84dfb00c5619567c7b2347aa4d0

lib/env.sh: fix iptables binary path
It should be under /usr/sbin instead of /usr/bin

Fixes: 51064b4a146f ("lib/env.sh: set iptables path")
Signed-off-by: Yujie Liu <yujie.liu@intel.com>
Signed-off-by: Philip Li <philip.li@intel.com>

lib/env.sh
@@ -93,13 +93,13 @@ set_iptables_path()
 	do
 		has_cmd $iptables_bin || {
 			if has_cmd $iptables_bin-nft; then
-				ln -sfv $(cmd_path $iptables_bin-nft) /usr/bin/$iptables_bin
-				ln -sfv $(cmd_path $iptables_bin-nft-restore) /usr/bin/$iptables_bin-restore
-				ln -sfv $(cmd_path $iptables_bin-nft-save) /usr/bin/$iptables_bin-save
+				ln -sfv $(cmd_path $iptables_bin-nft) /usr/sbin/$iptables_bin
+				ln -sfv $(cmd_path $iptables_bin-nft-restore) /usr/sbin/$iptables_bin-restore
+				ln -sfv $(cmd_path $iptables_bin-nft-save) /usr/sbin/$iptables_bin-save
 			elif has_cmd $iptables_bin-legacy; then
-				ln -sfv $(cmd_path $iptables_bin-legacy) /usr/bin/$iptables_bin
-				ln -sfv $(cmd_path $iptables_bin-legacy-restore) /usr/bin/$iptables_bin-restore
-				ln -sfv $(cmd_path $iptables_bin-legacy-save) /usr/bin/$iptables_bin-save
+				ln -sfv $(cmd_path $iptables_bin-legacy) /usr/sbin/$iptables_bin
+				ln -sfv $(cmd_path $iptables_bin-legacy-restore) /usr/sbin/$iptables_bin-restore
+				ln -sfv $(cmd_path $iptables_bin-legacy-save) /usr/sbin/$iptables_bin-save
 			fi
 		}
 	done