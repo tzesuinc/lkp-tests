52d69bac77d9fc5b93d59b36fd8cc4577d32f9b0
Philip Li <philip.li@intel.com>
2024-11-10 11:02:16 +0800

    lib/ruby.rb: define YAML unsafe load for Psych::VERSION < 4.0
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'psych'
+
 if RUBY_VERSION < '2.4'
   class Hash
     def transform_values
@@ -22,6 +24,17 @@ if RUBY_VERSION < '2.7'
       def absolute_path?(file_name)
         Pathname.new(file_name).absolute?
       end
+    end
+  end
+end
+
+if Psych::VERSION < '4.0'
+  require 'yaml'
+
+  module YAML
+    class << self
+      alias unsafe_load load
+      alias unsafe_load_file load_file
     end
   end
 end
