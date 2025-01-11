5f4bf1834d033b2888860a91f0598e5dadc56e5f
Philip Li <philip.li@intel.com>
2025-01-11 21:06:29 +0800

    lib/tests/lkvs.sh: correct the format to use tab
    Signed-off-by: Philip Li <philip.li@intel.com>

--- lib/tests/lkvs.sh
+++ lib/tests/lkvs.sh
@@ -6,114 +6,114 @@
 
 build_libipt()
 {
-    cd $BENCHMARK_ROOT/$testcase/libipt && cmake . && make install
+	cd $BENCHMARK_ROOT/$testcase/libipt && cmake . && make install
 }
 
 build_lkvs_tools()
 {
-    log_cmd cd $BENCHMARK_ROOT/$testcase/lkvs/BM/tools || return
+	log_cmd cd $BENCHMARK_ROOT/$testcase/lkvs/BM/tools || return
 
-    log_cmd make --keep-going || {
-        echo "tools make fail"
-        return 1
-    }
+	log_cmd make --keep-going || {
+		echo "tools make fail"
+		return 1
+	}
 
-    return 0
+	return 0
 }
 
 build_lkvs()
 {
-    build_lkvs_tools || return
+	build_lkvs_tools || return
 
-    [[ -f $BENCHMARK_ROOT/$testcase/lkvs/BM/$test/Makefile ]] || return 0
+	[[ -f $BENCHMARK_ROOT/$testcase/lkvs/BM/$test/Makefile ]] || return 0
 
-    cd $BENCHMARK_ROOT/$testcase/lkvs/BM/$test || return
+	cd $BENCHMARK_ROOT/$testcase/lkvs/BM/$test || return
 
-    if [[ $test = workload-xsave ]]; then
-        log_cmd mkdir build
-        log_cmd cd build
-        log_cmd cmake ..
-    fi
+	if [[ $test = workload-xsave ]]; then
+		log_cmd mkdir build
+		log_cmd cd build
+		log_cmd cmake ..
+	fi
 
-    log_cmd make --keep-going || {
-        echo "$test make fail"
-        return 1
-    }
+	log_cmd make --keep-going || {
+		echo "$test make fail"
+		return 1
+	}
 
-    return 0
+	return 0
 }
 
 fixup_tdx_compliance()
 {
-    log_cmd insmod tdx-compliance/tdx-compliance.ko
-    echo all > /sys/kernel/debug/tdx/tdx-tests
-    log_cmd cat /sys/kernel/debug/tdx/tdx-tests
+	log_cmd insmod tdx-compliance/tdx-compliance.ko
+	echo all > /sys/kernel/debug/tdx/tdx-tests
+	log_cmd cat /sys/kernel/debug/tdx/tdx-tests
 }
 
 fixup_splitlock()
 {
-    cat /proc/cpuinfo | grep -q split_lock_detect || die "split_lock_detect not supported on current CPU"
+	cat /proc/cpuinfo | grep -q split_lock_detect || die "split_lock_detect not supported on current CPU"
 }
 
 fixup_rapl_server()
 {
-    log_cmd modprobe -v intel_rapl_msr
+	log_cmd modprobe -v intel_rapl_msr
 }
 
 alias fixup_rapl_client=fixup_rapl_server
 
 runtests()
 {
-    cd $BENCHMARK_ROOT/$testcase/lkvs/BM || return
+	cd $BENCHMARK_ROOT/$testcase/lkvs/BM || return
 
-    if [[ $(type -t "fixup_${test//-/_}") =~ (alias|function) ]]; then
+	if [[ $(type -t "fixup_${test//-/_}") =~ (alias|function) ]]; then
 		fixup_${test//-/_} || return
-    fi
+	fi
 
-    if [[ -f $test/tests ]]; then
-        log_cmd ./runtests -f $test/tests
-    else
-        case $test in
-            cstate-client)
-                log_cmd ./runtests -f cstate/tests-client
-                ;;
-            cstate-server)
-                log_cmd ./runtests -f cstate/tests-server
-                ;;
-            fred)
-                log_cmd insmod fred/fred_test_driver.ko
-                # No doc about how to get the test result after loading the module
-                ;;
-            guest-test)
-                log_cmd ./runtests -f guest-test/guest.test_launcher.sh
-                ;;
-            prefetchi)
-                log_cmd prefetchi/prefetchi
-                ;;
-            rapl-client)
-                log_cmd ./runtests -f rapl/tests-client
-                ;;
-            rapl-server)
-                log_cmd ./runtests -f rapl/tests-server
-                ;;
-            th)
-                log_cmd ./runtests -c "th/th_test 1"
-                log_cmd ./runtests -c "th/th_test 2"
-                ;;
-            workload-xsave)
-                log_cmd cd $test/build || die "fail to cd build dir"
-                local available_workloads=$(./yogini 2>&1 | grep "Available workloads" | cut -d: -f 2 | xargs)
-                log_cmd ../start_test.sh -1 100 $available_workloads
-                ;;
-            topology-client)
-                log_cmd ./runtests -f topology/tests-client
-                ;;
-            topology-server)
-                log_cmd ./runtests -f topology/tests-server
-                ;;
-            *)
-                die "unknown $test"
-                ;;
-        esac
-    fi
+	if [[ -f $test/tests ]]; then
+		log_cmd ./runtests -f $test/tests
+	else
+		case $test in
+			cstate-client)
+				log_cmd ./runtests -f cstate/tests-client
+				;;
+			cstate-server)
+				log_cmd ./runtests -f cstate/tests-server
+				;;
+			fred)
+				log_cmd insmod fred/fred_test_driver.ko
+				# No doc about how to get the test result after loading the module
+				;;
+			guest-test)
+				log_cmd ./runtests -f guest-test/guest.test_launcher.sh
+				;;
+			prefetchi)
+				log_cmd prefetchi/prefetchi
+				;;
+			rapl-client)
+				log_cmd ./runtests -f rapl/tests-client
+				;;
+			rapl-server)
+				log_cmd ./runtests -f rapl/tests-server
+				;;
+			th)
+				log_cmd ./runtests -c "th/th_test 1"
+				log_cmd ./runtests -c "th/th_test 2"
+				;;
+			workload-xsave)
+				log_cmd cd $test/build || die "fail to cd build dir"
+				local available_workloads=$(./yogini 2>&1 | grep "Available workloads" | cut -d: -f 2 | xargs)
+				log_cmd ../start_test.sh -1 100 $available_workloads
+				;;
+			topology-client)
+				log_cmd ./runtests -f topology/tests-client
+				;;
+			topology-server)
+				log_cmd ./runtests -f topology/tests-server
+				;;
+			*)
+				die "unknown $test"
+				;;
+		esac
+	fi
 }
