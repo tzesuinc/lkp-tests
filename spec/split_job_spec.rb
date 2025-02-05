14ce32769c58934e99c79ac09335f7f7d872469c
Philip Li <philip.li@intel.com>
2025-02-05 16:51:51 +0800

    spec/split_job_spec.rb: enable all tests
    Signed-off-by: Philip Li <philip.li@intel.com>

--- spec/split_job_spec.rb
+++ spec/split_job_spec.rb
@@ -10,6 +10,10 @@ describe 'lkp-split-job' do
 
     `rsync -aix #{LKP_SRC}/ #{@tmp_src_dir}`
     `rsync -aix #{LKP_SRC}/spec/split-job/tests #{LKP_SRC}/spec/split-job/include #{@tmp_src_dir}/`
+
+    Dir.chdir(@tmp_src_dir) do
+      `bash -c "export LKP_SRC=#{@tmp_src_dir}; . #{@tmp_src_dir}/lib/host.sh; create_host_config"`
+    end
   end
 
   after(:all) do
@@ -24,9 +28,21 @@ describe 'lkp-split-job' do
     FileUtils.rm_rf @tmp_dir
   end
 
+  def execute_test(id)
+    `LKP_SRC=#{@tmp_src_dir} #{@tmp_src_dir}/bin/lkp split-job -t lkp-tbox -o #{@tmp_dir} spec/split-job/#{id}.yaml`
+
+    Dir[File.join(@tmp_dir, "#{id}-*.yaml")].each do |actual_yaml|
+      `sed -i 's/:#! /#!/g' #{actual_yaml}`
+
+      actual = YAML.load_file(actual_yaml)
+      expect = YAML.load_file("#{LKP_SRC}/spec/split-job/#{File.basename(actual_yaml)}")
+
+      expect(actual).to eq expect
+    end
+  end
+
   it 'split with --compatible option' do
     Dir.chdir(@tmp_src_dir) do
-      `bash -c "export LKP_SRC=#{@tmp_src_dir}; . #{@tmp_src_dir}/lib/host.sh; create_host_config"`
       `LKP_SRC=#{@tmp_src_dir} #{@tmp_src_dir}/bin/lkp split-job --compatible -o #{@tmp_dir} spec/split-job/compatible.yaml`
       new_yaml = 'compatible-test_1.yaml'
       # delete machine specific settings
@@ -38,43 +54,15 @@ describe 'lkp-split-job' do
     end
   end
 
-  xit "split job['split-job']['test'] only" do
-    Dir.chdir(@tmp_src_dir) do
-      `LKP_SRC=#{@tmp_src_dir} #{@tmp_src_dir}/bin/lkp split-job -t lkp-tbox -o #{@tmp_dir} spec/split-job/1.yaml`
-
-      Dir[File.join(@tmp_dir, '1-*.yaml')].each do |actual_yaml|
-        `sed -i 's/:#! /#!/g' #{actual_yaml}`
-        actual = YAML.load_file(actual_yaml)
-        expect = YAML.load_file("#{LKP_SRC}/spec/split-job/#{File.basename(actual_yaml)}")
-
-        expect(actual).to eq expect
-      end
-    end
+  it "split job['split-job']['test'] only" do
+    execute_test(1)
   end
 
-  xit "split job['split-job']['test'] and job['split-job']['group']" do
-    Dir.chdir(@tmp_src_dir) do
-      `LKP_SRC=#{@tmp_src_dir} #{@tmp_src_dir}/bin/lkp split-job -t lkp-tbox -o #{@tmp_dir} spec/split-job/2.yaml`
-      Dir[File.join(@tmp_dir, '2-*.yaml')].each do |actual_yaml|
-        `sed -i 's/:#! /#!/g' #{actual_yaml}`
-        actual = YAML.load_file(actual_yaml)
-        expect = YAML.load_file("#{LKP_SRC}/spec/split-job/#{File.basename(actual_yaml)}")
-
-        expect(actual).to eq expect
-      end
-    end
+  it "split job['split-job']['test'] and job['split-job']['group']" do
+    execute_test(2)
   end
 
-  xit "split job['fs'] only" do
-    Dir.chdir(@tmp_src_dir) do
-      `LKP_SRC=#{@tmp_src_dir} #{@tmp_src_dir}/bin/lkp split-job -t lkp-tbox -o #{@tmp_dir} spec/split-job/3.yaml`
-      Dir[File.join(@tmp_dir, '3-*.yaml')].each do |actual_yaml|
-        `sed -i 's/:#! /#!/g' #{actual_yaml}`
-        actual = YAML.load_file(actual_yaml)
-        expect = YAML.load_file("#{LKP_SRC}/spec/split-job/#{File.basename(actual_yaml)}")
-
-        expect(actual).to eq expect
-      end
-    end
+  it "split job['fs'] only" do
+    execute_test(3)
   end
 end
