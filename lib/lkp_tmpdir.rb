bf23891a3d0169f8bbfcaf570376dac2280c3f78
Philip Li <philip.li@intel.com>
2025-02-15 22:42:08 +0800

    lib/lkp_tmpdir.rb: add LKP::TmpDir class
    Signed-off-by: Philip Li <philip.li@intel.com>

---
+++ lib/lkp_tmpdir.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+LKP_SRC ||= ENV['LKP_SRC'] || File.dirname(__dir__)
+
+module LKP
+  class TmpDir
+    def initialize(prefix = nil)
+      @path = Dir.mktmpdir prefix
+      warn @path if ENV['debug'] == '1'
+
+      yield self if block_given?
+    end
+
+    def to_s
+      @path
+    end
+
+    def path(*args)
+      File.join([@path] + args)
+    end
+
+    def cleanup!
+      FileUtils.rm_r(@path) unless ENV['debug'] == '1'
+    end
+  end
+end
