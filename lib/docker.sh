tzesuinc/lkp-tests

commit 1a460f0139cf77ffd47753ffe68f676622c9c0a3

docker: support init of the test flow
Signed-off-by: Philip Li <philip.li@intel.com>

lib/docker.sh
@@ -1,0 +1,38 @@
+#!/bin/bash
+
+LKP_DOCKER_HOSTNAME="lkp-docker"
+LKP_DOCKER_TEST_DIR="/home/$USER/lkp"
+
+exec_cmd()
+{
+	echo "$@"
+	"$@"
+}
+
+load_container()
+{
+	local container="$1"
+	shift
+
+	[[ "$container" ]] || {
+		echo "Container file is not specified."
+		return 1
+	}
+
+	[[ "$container" == /* ]] || container=$LKP_DOCKER_TEST_DIR/containers/$container
+
+	[[ -f "$container" ]] || {
+		echo "Container file does not exist at $container."
+		return 1
+	}
+
+	local key value
+
+	while IFS='=' read -r key value; do
+		# trim any leading or trailing whitespace
+		key=$(echo $key | xargs)
+		value=$(echo $value | xargs)
+
+		[[ $value ]] && eval "opt_$key=$value"
+	done < $container
+}

docker/init
@@ -6,20 +6,16 @@ set -e
 
 . $LKP_SRC/lib/docker.sh
 
-while getopts "i:t:-:" opt; do
+while getopts "d:i:-:" opt; do
 	case $opt in
+		d)
+			opt_test_dir="$OPTARG"
+			;;
 		i)
 			opt_image="$OPTARG"
 			;;
-		t)
-			opt_container="$OPTARG"
-			;;
 		-)
 			case "${OPTARG}" in
-				container)
-					opt_container="${!OPTIND}"
-					OPTIND=$((OPTIND + 1))
-					;;
 				image)
 					opt_image="${!OPTIND}"
 					OPTIND=$((OPTIND + 1))
@@ -46,14 +42,19 @@ shift $((OPTIND - 1))
 opt_hostname=${opt_hostname:-$LKP_DOCKER_HOSTNAME}
 opt_test_dir=${opt_test_dir:-$LKP_DOCKER_TEST_DIR}
 
-[[ $opt_container ]] && load_container "$opt_container"
-
-[[ $opt_image ]] || {
+[[ "$opt_image" ]] || {
 	echo "No image specified. Use --image <image_name> to specify an image."
 	exit 1
 }
 
-cmd="docker build . -f $LKP_SRC/docker/${opt_image%%/*}/Dockerfile -t lkp-tests/${opt_image} --build-arg hostname=${opt_hostname} --build-arg base_image=${opt_image//\//:}"
-[[ $opt_proxy ]] && cmd="$cmd --build-arg http_proxy=${opt_proxy} --build-arg https_proxy=${opt_proxy}"
+containers="$opt_test_dir/containers"
+mkdir -p "$containers"
 
-exec_cmd $cmd
+cat <<EOF > "$containers/$opt_hostname"
+image=$opt_image
+hostname=$opt_hostname
+test_dir=$opt_test_dir
+proxy=$opt_proxy
+EOF
+
+echo "Container file is stored at $containers/$opt_hostname"

docker/run
@@ -1,17 +1,12 @@
 #!/bin/bash
 
+set -e
+
 [[ -n "$LKP_SRC" ]] || LKP_SRC=$(dirname $(dirname $(readlink -e -v $0)))
 
-exec_cmd()
-{
-	echo "$@"
-	"$@"
-}
+. $LKP_SRC/lib/docker.sh
 
-opt_hostname="lkp-docker"
-opt_test_dir="/home/$USER/lkp"
-
-while getopts "d:i:-:" opt; do
+while getopts "d:i:t:-:" opt; do
 	case $opt in
 		i)
 			opt_image="$OPTARG"
@@ -19,14 +14,25 @@ while getopts "d:i:-:" opt; do
 		d)
 			opt_test_dir="$OPTARG"
 			;;
+		t)
+			opt_container="$OPTARG"
+			;;
 		-)
 			case "${OPTARG}" in
+				container)
+					opt_container="${!OPTIND}"
+					OPTIND=$((OPTIND + 1))
+					;;
 				image)
 					opt_image="${!OPTIND}"
 					OPTIND=$((OPTIND + 1))
 					;;
 				hostname)
 					opt_hostname="${!OPTIND}"
+					OPTIND=$((OPTIND + 1))
+					;;
+				test-dir)
+					opt_test_dir="${!OPTIND}"
 					OPTIND=$((OPTIND + 1))
 					;;
 				recreate)
@@ -43,13 +49,23 @@ done
 
 shift $((OPTIND - 1))
 
+opt_hostname=${opt_hostname:-$LKP_DOCKER_HOSTNAME}
+opt_test_dir=${opt_test_dir:-$LKP_DOCKER_TEST_DIR}
+
+[[ $opt_container ]] && load_container "$opt_container"
+
 docker inspect $opt_hostname > /dev/null 2>&1 && {
 	echo "Container $opt_hostname already exists."
 
 	[[ "$opt_recreate" = true ]] || exit 0
 
-	exec_cmd docker rm -f $opt_hostname || exit
+	exec_cmd docker rm -f $opt_hostname
 }
+
+for subdir in paths benchmarks result jobs
+do
+	mkdir -p "$opt_test_dir/$subdir"
+done
 
 exec_cmd docker run -d -h $opt_hostname --name $opt_hostname \
 	-v $opt_test_dir/paths:/lkp/paths \

docker/test
@@ -2,16 +2,11 @@
 
 [[ -n "$LKP_SRC" ]] || LKP_SRC=$(dirname $(dirname $(readlink -e -v $0)))
 
-exec_cmd()
-{
-	echo "$@"
-	"$@"
-}
+set -e
 
-opt_hostname="lkp-docker"
-opt_test_dir="/home/$USER/lkp"
+. $LKP_SRC/lib/docker.sh
 
-while getopts "d:i:j:g:-:" opt; do
+while getopts "d:i:j:g:t:-:" opt; do
 	case $opt in
 		i)
 			opt_image="$OPTARG"
@@ -25,14 +20,29 @@ while getopts "d:i:j:g:-:" opt; do
 		g)
 			opt_pattern="$OPTARG"
 			;;
+		t)
+			opt_container="$OPTARG"
+			;;
 		-)
 			case "${OPTARG}" in
+				container)
+					opt_container="${!OPTIND}"
+					OPTIND=$((OPTIND + 1))
+					;;
 				image)
 					opt_image="${!OPTIND}"
 					OPTIND=$((OPTIND + 1))
 					;;
 				hostname)
 					opt_hostname="${!OPTIND}"
+					OPTIND=$((OPTIND + 1))
+					;;
+				job-yaml)
+					opt_job_yaml="${!OPTIND}"
+					OPTIND=$((OPTIND + 1))
+					;;
+				pattern)
+					opt_pattern="${!OPTIND}"
 					OPTIND=$((OPTIND + 1))
 					;;
 				proxy)
@@ -53,14 +63,20 @@ done
 
 shift $((OPTIND - 1))
 
-[[ "$opt_job_yaml" ]] || {
+opt_hostname=${opt_hostname:-$LKP_DOCKER_HOSTNAME}
+opt_test_dir=${opt_test_dir:-$LKP_DOCKER_TEST_DIR}
+
+[[ $opt_container ]] && load_container "$opt_container"
+
+[[ $opt_job_yaml ]] || {
 	echo "No job yaml specified. Use -j <job_yaml> to specify a job yaml."
 	exit 1
 }
 
 cmd="$LKP_SRC/docker/run -i $opt_image --hostname $opt_hostname -d $opt_test_dir"
 [[ "$opt_recreate" = true ]] && cmd="$cmd --recreate"
-exec_cmd $cmd || exit
+
+exec_cmd $cmd
 
 test()
 {

commit e8e5a5d61ca7d25d8a7501ea355d2308b73cf740

lkp-exec/docker: resolve lkp init/run doesn't work
Fixes: 1a460f0139cf ("docker: support init of the test flow")
Signed-off-by: Philip Li <philip.li@intel.com>

lib/docker.sh
@@ -35,4 +35,6 @@ load_container()
 
 		[[ $value ]] && eval "opt_$key=$value"
 	done < $container
+
+	return 0
 }

lkp-exec/docker
@@ -6,7 +6,11 @@ command="$1"
 shift
 
 case "$command" in
-	build|test)
+	build|init|run|test)
 		$LKP_SRC/docker/$command "$@"
+		;;
+	*)
+		echo "Unknown command $command"
+		exit 1
 		;;
 esac