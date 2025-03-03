4f9cbb5840e756e358b03a624fe2a1f305a73c73
Alwin Low Min Wai <alwin.min.wai.low@intel.com>
2025-03-03 23:37:32 +0800

    Fix issue 424
    Signed-off-by: Alwin Low Min Wai <alwin.min.wai.low@intel.com>

--- spec/filter_need_memory_spec.rb
+++ spec/filter_need_memory_spec.rb
@@ -8,8 +8,9 @@ system_free_mem_gb = Integer(Bash.call("free -g | sed -n '2, 1p' | awk '{print $
 
 describe 'filters/need_memory' do
   before(:all) do
-    @tmp_dir = Dir.mktmpdir(nil, '/tmp')
-    FileUtils.chmod 'go+rwx', @tmp_dir
+    @tmp_dir = LKP::TmpDir.new('filter-need-memory-spec-')
+    @tmp_dir.add_permission
+    @tmp_dir = @tmp_dir.to_s
 
     @test_yaml_file = "#{@tmp_dir}/test.yaml".freeze
   end
@@ -18,32 +19,30 @@ describe 'filters/need_memory' do
     FileUtils.remove_entry @tmp_dir
   end
 
+  def generate_job(contents)
+    File.open(@test_yaml_file, 'w') do |f|
+      f.write(contents.to_yaml)
+    end
+    Job.open(@test_yaml_file)
+  end
+
   context 'when do not have need_memory' do
     it 'does not filter the job' do
-      File.open(@test_yaml_file, 'w') do |f|
-        f.write({ 'testcase' => 'testcase' }.to_yaml)
-      end
-      job = Job.open(@test_yaml_file)
+      job = generate_job({ 'testcase' => 'testcase' })
       job.expand_params
     end
   end
 
   context 'when need_memory smaller than available_memory' do
     it 'does not filter the job' do
-      File.open(@test_yaml_file, 'w') do |f|
-        f.write({ 'testcase' => 'testcase', 'need_memory' => "#{system_free_mem_gb - 1}G" }.to_yaml)
-      end
-      job = Job.open(@test_yaml_file)
+      job = generate_job({ 'testcase' => 'testcase', 'need_memory' => "#{system_free_mem_gb - 1}G" })
       job.expand_params
     end
   end
 
   context 'when need_memory larger than available_memory' do
     it 'filter the job' do
-      File.open(@test_yaml_file, 'w') do |f|
-        f.write({ 'testcase' => 'testcase', 'need_memory' => '100%', 'nr_cpu' => system_free_mem_gb + 2 }.to_yaml)
-      end
-      job = Job.open(@test_yaml_file)
+      job = generate_job({ 'testcase' => 'testcase', 'need_memory' => '100%', 'nr_cpu' => system_free_mem_gb + 2 })
       expect { redirect_to_string { job.expand_params } }.to raise_error Job::ParamError
     end
   end
