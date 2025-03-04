78d889edf2c4fd829e998b446f0b655968ccf672
Chiam Jia Ying <jia.ying.chiam@intel.com>
2025-03-04 10:53:06 +0800

    spec/filter_need_kconfig_spec.rb: refactor by LKP::TmpDir
    Added generate_job method
    
    Closes #421
    
    Signed-off-by: Wei Shan <wei.shan.lim@intel.com>
    Signed-off-by: Ang Yee Teng <yee.teng.ang@intel.com>
    Co-authored-by: Wei Shan <wei.shan.lim@intel.com>

--- spec/filter_disk_spec.rb
+++ spec/filter_disk_spec.rb
@@ -6,52 +6,48 @@ require "#{LKP_SRC}/lib/bash"
 
 describe 'filter/disk' do
   before(:all) do
-    @tmp_dir = Dir.mktmpdir(nil, '/tmp')
-    FileUtils.chmod 'go+rwx', @tmp_dir
+    @tmp_dir = LKP::TmpDir.new('filter-disk-spec-rb-')
+    @tmp_dir.add_permission
 
-    @test_yaml_file = "#{@tmp_dir}/test.yaml".freeze
+    @test_yaml_file = @tmp_dir.path('test.yaml').freeze
   end
 
   after(:all) do
-    FileUtils.remove_entry @tmp_dir
+    @tmp_dir.cleanup!
+  end
+
+  def generate_job(contents)
+    File.open(@test_yaml_file, 'w') do |f|
+      f.write(contents.to_yaml)
+    end
+  
+    Job.open(@test_yaml_file)
   end
 
   context 'when do not need disk' do
     it 'does not filter the job' do
-      File.open(@test_yaml_file, 'w') do |f|
-        f.write({ 'testcase' => 'testcase' }.to_yaml)
-      end
-      job = Job.open(@test_yaml_file)
+      job = generate_job({ 'testcase' => 'testcase' })
       job.expand_params
     end
   end
 
   context 'when disk: 1HDD, nr_hdd_partitions: 1' do
     it 'does not filter the job' do
-      File.open(@test_yaml_file, 'w') do |f|
-        f.write({ 'testcase' => 'testcase', 'nr_hdd_partitions' => '1', 'disk' => '1HDD' }.to_yaml)
-      end
-      job = Job.open(@test_yaml_file)
+      job = generate_job({ 'testcase' => 'testcase', 'nr_hdd_partitions' => '1', 'disk' => '1HDD' })
       job.expand_params
     end
   end
 
   context 'when disk: 4HDD, nr_hdd_partitions: 1' do
     it 'filter the job' do
-      File.open(@test_yaml_file, 'w') do |f|
-        f.write({ 'testcase' => 'testcase', 'nr_hdd_partitions' => '1', 'disk' => '4HDD' }.to_yaml)
-      end
-      job = Job.open(@test_yaml_file)
+      job = generate_job({ 'testcase' => 'testcase', 'nr_hdd_partitions' => '1', 'disk' => '4HDD' })
       expect { redirect_to_string { job.expand_params } }.to raise_error Job::ParamError
     end
   end
 
   context 'when disk: 1HDD, do not have hdd_partition' do
     it 'filter the job' do
-      File.open(@test_yaml_file, 'w') do |f|
-        f.write({ 'testcase' => 'testcase', 'disk' => '1HDD' }.to_yaml)
-      end
-      job = Job.open(@test_yaml_file)
+      job = generate_job({ 'testcase' => 'testcase', 'disk' => '1HDD' })
       expect { redirect_to_string { job.expand_params } }.to raise_error Job::ParamError
     end
   end
