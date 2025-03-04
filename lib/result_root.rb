tzesuinc/lkp-tests

commit cd7dd4efac223570c6be332dcf80d3a617910bad

lib/result_root.rb: resolve rubocop Naming/AccessorMethodName issue
Fix Issue #402

Signed-off-by: Ang Yee Teng <yee.teng.ang@intel.com>
Signed-off-by: Ong Yen Yee <yen.yee.ong@intel.com>
Co-authored-by: Ang Yee Teng <yee.teng.ang@intel.com>

lib/result_root.rb
@@ -83,6 +83,9 @@ end
 class ResultRootCollection
   include Enumerable
 
+  attr_writer :date_glob
+  attr_writer :queue
+
   def initialize(conditions = {})
     @conditions = conditions
     @date_glob = nil
@@ -98,16 +101,8 @@ class ResultRootCollection
     self
   end
 
-  def set_date_glob(glob)
-    @date_glob = glob
-  end
-
-  def set_date(time)
+  def apply_date(time)
     @date_glob = str_date(time)
-  end
-
-  def set_queue(queue)
-    @queue = queue
   end
 
   def each

commit 8046fc6db4047b1dac8ebea22fab1219eae1247a

lib/result_root.rb: fix the invalid call of set_date in convert_all_mresult_root()
Fixes: cd7dd4efac22 ("lib/result_root.rb: resolve rubocop Naming/AccessorMethodName issue")
Signed-off-by: Philip Li <philip.li@intel.com>

lib/result_root.rb
@@ -83,8 +83,7 @@ end
 class ResultRootCollection
   include Enumerable
 
-  attr_writer :date_glob
-  attr_writer :queue
+  attr_writer :date_glob, :queue
 
   def initialize(conditions = {})
     @conditions = conditions
@@ -353,7 +352,7 @@ def convert_all_mresult_root(date_from_in = nil, date_to_in = nil)
   date_to = date_of_time date_to_in
   n = 0
   while date <= date_to
-    rtc.set_date(date)
+    rtc.apply_date(date)
     rtc.each do |rt|
       n += 1
       _rt = rt._result_root